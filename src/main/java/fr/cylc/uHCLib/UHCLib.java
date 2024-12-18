2d3c9770ad2efae06b5d9fa1a35e395ce3d85b6d
Loïc Cambray <pro.loiccambray@gmail.com>
2024-12-18 09:30:12 +0100

    first commit

---
+++ src/main/java/fr/cylc/uHCLib/UHCLib.java
@@ -0,0 +1,17 @@
+package fr.cylc.uHCLib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UHCLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
